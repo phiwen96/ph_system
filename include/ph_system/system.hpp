e1d76a19516c6f44560ea7dc6954a95ca791ae35
Philip Wenkel <38218064+phiwen96@users.noreply.github.com>
2021-07-28 20:01:58 +0200

    a

--- include/ph_system/system.hpp
+++ include/ph_system/system.hpp
@@ -2,14 +2,16 @@
 
 namespace ph::system
 {
-    
-    inline constexpr bool Darwin = true;
-    
-    inline constexpr bool Linux = false;
-    
-    inline constexpr bool Windows = false;
-    
-    inline constexpr bool X86_64 = false;
-    
-    inline constexpr bool Arm64 = true;
+    inline namespace v0_0_0
+    {
+        inline constexpr bool Darwin = true;
+        
+        inline constexpr bool Linux = false;
+        
+        inline constexpr bool Windows = false;
+        
+        inline constexpr bool X86_64 = false;
+        
+        inline constexpr bool Arm64 = true;
+    }
 }
