1015c4f69a357d0934ae291cb962ad2cc2c1a89d
Philip Wenkel <38218064+phiwen96@users.noreply.github.com>
2021-07-28 19:56:33 +0200

    a

--- tests/test.cpp
+++ tests/test.cpp
@@ -8,13 +8,20 @@ using namespace std;
 
 TEST_CASE ("")
 {
-    constexpr auto res = ph::system::Darwin or ph::system::Arm64 or ph::system::Linux or ph::system::Windows or ph::system::X86_64;
+    constexpr auto res = ph::system::Darwin or ph::system::Linux or ph::system::Windows;
+    constexpr auto res2 = ph::system::Arm64 or ph::system::X86_64;
     
     REQUIRE (res);
+    REQUIRE (res2);
+    
+    
     
     cout << "Darwin" << setw (10) << ph::system::Darwin << endl;
-    cout << "Arm64" << setw (10) << ph::system::Arm64 << endl;
     cout << "Linux" << setw (10) << ph::system::Linux << endl;
-    cout << "Windows" << setw (10) << ph::system::Windows << endl;
+    cout << "Windows" << setw (10) << ph::system::Windows << endl << endl;
+    
+    cout << "Arm64" << setw (10) << ph::system::Arm64 << endl;
     cout << "X86_64" << setw (10) << ph::system::X86_64 << endl;
+    
+    
 }
